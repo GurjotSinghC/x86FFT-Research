50e79a323f73bb1f4d75279994dedb552d4759da
GurjotSinghC <161785025+GurjotSinghC@users.noreply.github.com>
2025-07-31 17:25:05 -0700

    Add files via upload

--- MoreThanJustFFT.asm
+++ MoreThanJustFFT.asm
@@ -6,18 +6,28 @@ _O_RDONLY: 	= 0x0000
 _O_TEXT: 	= 0x4000
 
 	#Debug Strings I use this to prefix values I need to view in console
+
 realprint:	.asciiz	"This is the Real component "
 imagprint:	.asciiz	"imaginary component  "
 firstphit:	.asciiz	"We Set the First PhIT  "
 firstph2:	.asciiz	"This is the real and imagine of phit^2:  "
+
 compadd:	.asciiz	"Result of a complex addition  "
 compsub:	.asciiz "Result of a complex subtraction  "
 tandt:		.asciiz	"x[b] = t * T = "
+
 Data:		.double	1,0,2,0,3,0,4,0,5,0,6,0,7,0,8,0
+
+
+
+
+
 complex: 	.struct
 real:		.double	0.0
 imaginary:	.double	0.0
 # powerofTwo:	.double	2 cant remember what this is for
+
+
 	.code
 complex.add:
 # complex:v0 complex::add(complex*arg1:a0,complex*arg2:a1)
@@ -35,6 +45,11 @@ complex.add:
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
 	sw	$a3,fp.a3($fp)
+
+
+
+
+
 	l.d	$f12,complex.real($a0)
 	l.d	$f14,complex.imaginary($a0)
 	l.d	$f16,complex.real($a1)
@@ -43,13 +58,22 @@ complex.add:
 	add.d	$f2,$f14,$f18
 	s.d	$f0,complex.real($v0)
 	s.d	$f2,complex.imaginary($v0)
+
 	la	$a0,compadd
 	syscall	$print_string
+
 	mov.d	$f12,$f0
+
 	syscall	$print_double
+
 	li	$a0,' 
-	syscall	$print_char	
-	mov.d	$f12,$f2	
+	syscall	$print_char
+	
+	mov.d	$f12,$f2
+	
+
+
+
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -63,6 +87,7 @@ complex.add:
 
 complex.mul:
 #complex:v0 complex::mul(complex * arg1:$a0,complex*arg2:a1)
+
 	addi	$sp,$sp,fp.s4-fp.a4	# standard stack frame
 	sw	$fp,fp.fp-fp.s4($sp)
 	add	$fp,$sp,fp.fp-fp.s4
@@ -76,6 +101,12 @@ complex.mul:
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
 	sw	$a3,fp.a3($fp)
+
+
+
+
+
+
 	l.d	$f12,complex.real($a0)
 	l.d	$f14,complex.imaginary($a0)
 	l.d	$f16,complex.real($a1)
@@ -99,10 +130,13 @@ complex.mul:
 	li	$a0,' 
 	syscall	$print_char
 	s.d	$f2,complex.imaginary($v0)
+
 	la	$a0,imagprint
 	syscall	$print_string
 	mov.d	$f12,$f2
+
 	syscall	$print_double
+
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -130,6 +164,12 @@ complex.sub:
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
 	sw	$a3,fp.a3($fp)
+
+
+
+
+
+
 	l.d	$f12,complex.real($a0)
 	l.d	$f14,complex.imaginary($a0)
 
@@ -139,6 +179,8 @@ complex.sub:
 	sub.d	$f2,$f14,$f18
 	s.d	$f0,complex.real($v0)
 	s.d	$f2,complex.imaginary($v0)
+
+
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -151,6 +193,7 @@ complex.sub:
 
 complex.div:
 #complex:v0 complex::div(complex*arg1:a0,complex*arg2:a1)
+
 	addi	$sp,$sp,fp.s4-fp.a4	# standard stack frame
 	sw	$fp,fp.fp-fp.s4($sp)
 	add	$fp,$sp,fp.fp-fp.s4
@@ -164,23 +207,39 @@ complex.div:
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
 	sw	$a3,fp.a3($fp)
+
+
+
+
+
+
 	l.d	$f12,complex.real($a0)
 	l.d	$f14,complex.imaginary($a0)
+
 	l.d	$f16,complex.real($a1)
 	l.d	$f18,complex.imaginary($a1)
+
 	mul.d	$f0,$f12,$f16 	# a*c
 	mul.d	$f2,$f14,$f18 	# b*d
 	add.d	$f0,$f0,$f2 	# ac + bd
+
 	mul.d	$f2,$f14,$f16	# bc
 	mul.d	$f4,$f12,$f18	# ad
 	sub.d	$f2,$f2,$f4	# bc-ad
+
 	mul.d	$f4,$f16,$f16	#c²
 	mul.d	$f8,$f18,$f18	#d²
+
 	add.d	$f4,$f4,$f8	#c²+d²
+
 	div.d	$f0,$f0,$f4	# whole real term
+
 	div.d	$f2,$f2,$f4	# whole imaginary term
+
 	s.d	$f0,complex.real($v0)
 	s.d	$f2,complex.imaginary($v0)
+
+
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -191,25 +250,41 @@ complex.div:
 	add	$sp,$sp,fp.a4-fp.s4
 	jr	$ra
 
+
+
+
+
+
+
+
 		.data
 file:		.struct	
 readhead:	.word	0
 	.data
 file.name:	.asciiz	"Data.txt"
+
 	.align	4
+
 	.code
 file.readfile:	
 
+	
 	li	$a0,2000
+	
 	syscall	$malloc
 # 	MIPS  P8700 Multiprocessing System (MPS) cost
 	sw	$v0,Datapointer
 	la	$t0,Datapointer
+
 	mov	$a0,$s0	#file handle as first argument
 	la	$a1,Datapointer
 	li	$a2,1024 #number of bytes to be read
+
 	syscall	$read
-	mov	$a0,$s0	
+
+	mov	$a0,$s0
+	
+
 	mov	$t2,$v0	#this is the nunumber of bytes read 
 # 	add	$t2,$t0,$t2	#this is teh last address of the buffer
 	syscall	$close
@@ -219,6 +294,7 @@ file.readfile:
 	# ($t3) is the number of bytes read
 	# ($s0) holds the file handle
 	# ($t8) is my for loop counter
+
 	li	$t7,'\n
 	li	$t4,0
 	li	$t6,'\n
@@ -226,6 +302,7 @@ file.readfile:
 1:	
 #  	beq	$t8,$t2,2f
 	lb	$t5,($t0)	
+	
 	bne	$t5,$t6,iterate	
 nullwrite:
 	sb	$0,($t0)
@@ -254,10 +331,15 @@ comparisontable:	.double	1.0e-100,1.0e-10,1.0e-1,1.0,10.0,1.0e10,1.0e100
 	.code
 #value to to convert in $a0
 #
-file.writefile_one_double:	
+file.writefile_one_double:
+
+
+	
 # 	l.d	$f2,comparisontable
 	
 # 	exponent in base 10 actual base of our floating point system
+
+
 	addi	$sp,$sp,fp.s4-fp.a4	# standard stack frame
 	sw	$fp,fp.fp-fp.s4($sp)
 	add	$fp,$sp,fp.fp-fp.s4
@@ -271,7 +353,13 @@ file.writefile_one_double:
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
 	sw	$a3,fp.a3($fp)
+
+
 	#start it off by checking for zero
+
+
+
+
 	la	$t0,comparisontable #
 	addi	$t0,double*3 # get the first value of 1 to compare	
 	l.d	$f2,($t0)	#load the first comparison table value
@@ -281,17 +369,21 @@ file.writefile_one_double:
 
 less_than_one:
 	la	$t0,comparisontable	#get the lowest value
+
 	l.d	$f2,($t0)		#gets 10^-100	
 	li	$t2,100
 	mtc1	$t2,$f8		#load 100 inot f4 this is our running exponent addition
 	cvt.w.d	$f8,$f8	
+
 	c.lt.d	$f0,$f2
 	bc1t	left_to_right1
+
 commence_division:
 1:	div.d	$f12,$f0,$f2	# divides input by 10^100	
 	add.d	$f4,$f4,$f8	#add running total of exponent
 	c.lt.d	$f2,$f0
 	bc1f	1b
+
 left_to_right1:
 	addi	$t0,8	#gets to the 10^10
 	li	$t2,10
@@ -299,6 +391,10 @@ left_to_right1:
 	cvt.w.d	$f8,$f8
 	c.lt.d	$f0,$f2
 # 	bc1t	left_to_right2
+
+
+
+
 greater_than_one:
 	li	$t2,100
 	mtc1	$t2,$f8		#load 100 inot f4 this is our running exponent addition
@@ -359,13 +455,22 @@ endingsequence:
 	mul.d	$f12,$f12,$f14
 	addi	$t6,-1
 	bne	$t6,$t7,1b
+	
+
 	#look at f8 if its 0 ignore it 
 	#if its not zero than you output an e output the sign of f8 
 	#take the abosulate value and convert 
-	#possibly thrtee digit number 	
- 	#check fi tits zero
-	# 100*first digit divided by 10 after 10s you got one digit left
+	#possibly thrtee digit number 
 	
+ #check fi tits zero
+
+	
+# 100*first digit divided by 10 after 10s you got one digit left
+	
+
+
+
+
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -377,6 +482,18 @@ endingsequence:
 	jr	$ra
 
 	.data
+
+
+
+
+
+
+
+
+
+
+
+
 PI:	.double	3.1415926535897931
 # use atan 1 compe up with pi 
 T:	.double	1
@@ -386,9 +503,16 @@ N:	.word	8
 ThetaT:	.double	0
 phiT:	.double	0
 	.double	0
+
 Datapointer:	.word	0
+
 t:	.double	0,0
 m:	.word	3
+
+
+
+
+
 	.data
 fp:	.struct	-36
 loc:	.word	0
@@ -414,8 +538,21 @@ a7:	.word	0
 
 	
 	.code
+
+
+
+
+
+
+
+
+
+
+
+
 				# // (Your fft, ifft, mul_poly, sdp, int_sdp functions go here,
-				# // copied exactly from your provided code) 
+				# // copied exactly from your provided code)
+				# 
 				# // Cooley-Tukey FFT (in-place, breadth-first, decimation-in-frequency)
 				# // Better optimized but less intuitive
 				# // !!! Warning : in some cases this code make result different from not optimased version above (need to fix bug)
@@ -436,34 +573,52 @@ complex.Cooley_Tukey_FFT:
 	sw	$a0,fp.a0($fp)
 	sw	$a1,fp.a1($fp)
 	sw	$a2,fp.a2($fp)
-	sw	$a3,fp.a3($fp)				# void fft(CArray &x,unsigned int m)
+	sw	$a3,fp.a3($fp)
+
+
+
+
+				# void fft(CArray &x,unsigned int m)
 				# {
 				#     // DFT
 				#     unsigned int N = x.size(), k = N, n;
+
 	lw	$t0,N		# N = size
 	mov	$t1,$t0		# k = N
 	li	$t2,0		#n = 0	
+
 				#     double thetaT = PI / N; // division done in Long double output into double
 	l.d	$f0,PI
-	l.d	$f2,Nfloat	
+	l.d	$f2,Nfloat
+	
 	div.d	$f4,$f0,$f2
 	s.d	$f4,ThetaT
-				#     Complex phiT = Complex(cos(thetaT), -sin(thetaT)), T;	
-	#f4 has thetaT	
+				#     Complex phiT = Complex(cos(thetaT), -sin(thetaT)), T;
+	
+	#f4 has thetaT
+	
 	cos.d	$f6,$f4
+	
 	sin.d	$f8,$f4
 	neg.d	$f8,$f8
+
+
 	s.d	$f6,phiT
 	la	$t9,phiT
 	s.d	$f8,8($t9)	#basically storing to the double next phiT in memory
+
 	li	$a0,'\n
 	syscall	$print_char
+
 	li	$a0,'\n
 	syscall	$print_char
+
 	la	$a0,firstphit
-	syscall	$print_string	
+	syscall	$print_string
+	
 	l.d	$f12,phiT
 	syscall	$print_double
+
 	l.d	$f12,8($t9)
 	syscall	$print_double
 	#t9 is now phiT address
@@ -478,20 +633,26 @@ complex.Cooley_Tukey_FFT:
 top:	
 
 	blez	$t1,whileend
-					#         n = k;
+	#         n = k;
 	mov	$t2,$t1
 	#         k >>= 1;
 	srl	$t1,$t1,1
 #         phiT = phiT * phiT;
-	la	$v0,phiT  		#output stored here
+	
+	la	$v0,phiT  #output stored here
 	la	$a0,phiT
 	la	$a1,phiT
+	
 	addi	$sp,-4
 	sw	$ra,($sp)
+
 	jal	complex.mul
-# 	s.d	$v0,phiT		# this si the result of phit * phit
+
+# 	s.d	$v0,phiT	# this si the result of phit * phit
+	
 	lw	$ra,($sp)
 	addi	$sp,4
+
 	li	$a0,'\n
 	syscall	$print_char
 	la	$a0,firstph2
@@ -513,34 +674,49 @@ top:
 # 	l.d	$f12,($a0)
 # 	
 # 	syscall	$print_double
+
+
+
 	#did this in data section#         T = 1.0L;
 	li	$t9,0 #l =0				
 tloop:				#         for (unsigned int l = 0; l < k; l++)
 	
-	bgt	$t9,$t1,tloopend	
+	bgt	$t9,$t1,tloopend
+	
+	
 				#         {floop
 				#             for (unsigned int a = l; a < N; a += n)
+
 	mov	$t8,$t9	#a = l
 sloop:	bgt	$t8,$t0,sloopend
 	beq	$t8,$t0,sloopend
 
 				#             {sloop
 				#                 unsigned int b = a + k;
-	add	$t3,$t8,$t1				#                 Complex t = x[a] - x[b];
+	add	$t3,$t8,$t1
+				#                 Complex t = x[a] - x[b];
 	la	$t7,Data #array
 # 	mov	$s0,$t8
 	li	$s1,16
 	mul	$s0,$t8,$s1
 # 	mflo	$s0
-	add	$a0,$t7,$s0 #x[a]	
+
+	add	$a0,$t7,$s0 #x[a]
+	
 # 	mov	$s0,$t3	#b
+
 	mul	$s0,$t3,$s1	# multily b by 16
+
 # 	mflo	$s0
 	add	$a1,$t7,$s0
+
 	addi	$sp,-4
 	sw	$ra,($sp)
-	la	$v0,t	#need to know the address of the output	
-	jal	complex.sub #cahnged from mul	
+
+	la	$v0,t	#need to know the address of the output
+	
+	jal	complex.sub #cahnged from mul
+	
 	lw	$ra,($sp)
 	addi	$sp,4
 	la	$s3,t
@@ -548,15 +724,21 @@ sloop:	bgt	$t8,$t0,sloopend
 	la	$a0,compsub
 	syscall	$print_string
 	syscall	$print_double
+
+
 # 	sw	$v0,t	# this holds complex t
 				#                 x[a] += x[b];
 	mov	$s0,$t8	#t8 is a
 	li	$s1,16
 	mul	$s0,$s0,$s1 #byte index a
 # 	mflo	$s0
+
 	add	$a0,$t7,$s0 #x[a]
 	mov	$v0,$a0	#fixes
+
+
 	#loading xb
+
 	mov	$s0,$t3	#b
 	mul	$s0,$s0,$s1	# multily b by 16
 # 	mflo	$s0 forgot how mul works
@@ -565,7 +747,10 @@ sloop:	bgt	$t8,$t0,sloopend
 	sw	$ra,($sp)
 	jal	complex.add
 	lw	$ra,($sp)
-	addi	$sp,4			#                 x[b] = t * T;
+	addi	$sp,4
+	
+
+				#                 x[b] = t * T;
 	mov	$s0,$t3	#b
 	mul	$s0,$s0,$s1	# multily b by 16
 # 	mflo	$s0	# corrected indice
@@ -577,6 +762,7 @@ sloop:	bgt	$t8,$t0,sloopend
 	jal	complex.mul
 	lw	$ra,($sp)
 	addi	$sp,4
+
 	la	$a0,tandt
 	syscall	$print_string
 	l.d	$f12,($v0)
@@ -584,7 +770,8 @@ sloop:	bgt	$t8,$t0,sloopend
 	add	$t8,$t8,$t2 #a+=n
 	j	sloop
 sloopend:	#             }sloop
-						#             T *= phiT;
+			
+				#             T *= phiT;
 	la	$a0,T
 	la	$a1,phiT
 	la	$v0,T
@@ -594,11 +781,14 @@ sloopend:	#             }sloop
 	lw	$ra,($sp)
 	addi	$sp,4
 	addi	$t9,1
+
 # 
 # 	li	$a0,'S
 # 	syscall	$print_char
 # 	l.d	$f12,T
 # 	syscall	$print_double
+
+
 	j	tloop			#         }floop
 
 tloopend:
@@ -610,9 +800,14 @@ tloopend:
 whileend:
 				#     // Decimate
 				#     //    unsigned int m = (unsigned int)log2(N);
-			#     
-	li	$t8,0 #a=0				#{
+
+			
+				#     
+	li	$t8,0 #a=0
+				#{
 	lw	$s7,N
+
+
 	li	$a0,'V
 	syscall	$print_char
 	l.d	$f12,phiT
@@ -620,9 +815,12 @@ whileend:
 Decimate:
 bga:
 	bge	$t8,$s7,FinishDecimate	#     for (unsigned int a = 0; a < N; a++)
+	
+	
 				#         unsigned int b = a;
 	mov	$t3,$t8
 				#         // Reverse bits
+	
 				#         b = (((b & 0xaaaaaaaa) >> 1) | ((b & 0x55555555) << 1));
 	andi	$t9,$t3,0xaaaaaaaa
 	srl	$t9,$t9,1
@@ -635,7 +833,8 @@ bga:
 	andi	$s2,$t3,0x33333333
 	sll	$s2,$s2,2
 	or	$t3,$t9,$s2
-			#         b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
+
+				#         b = (((b & 0xf0f0f0f0) >> 4) | ((b & 0x0f0f0f0f) << 4));
 	andi	$t9,$t3,0xf0f0f0f0
 	srl	$t9,$t9,4
 	andi	$s2,$t3,0x0f0f0f0f
@@ -658,58 +857,92 @@ bga:
 #     	andi    $s0,$t3,0x0000ffff    # Extract lower 16 bits of b
 #     	sll     $s0,$s0,16            # Shift lower 16 bits to upper 16 bits
 #     	or      $t3,$t9,$s0           # Combine the shifted halves into b ($t3)
+
 	lw	$v1,m
 	neg	$v1,$v1
 	addi	$s1,$v1,32
+
+
 	srl	$t3,$t9,$s1
+
 	addi	$t8,1
+
+
 # bga:				#         if (b > a)
 				#         {
 	blt	$t3,$t8,2f
+
+	
 1:				#             //            Complex t = x[a];
 # 	la	$t7,Data
 	li	$s7,16
 	mul	$s4,$s7,$t8
 # 	mflo	$s7
 	add	$s4,$t7,$s4
+
 	l.d	$f0,($s4)
 	s.d	$f0,t
 	l.d	$f2,t
-			#             //            x[a] = x[b];
+				#             //            x[a] = x[b];
+
 	li	$s7,16
 # 	add	$s7,$s7,$t3 #S7 IS A PROPER INDEX
+	
+	
+
 	mul	$s7,$s7,$t3
 # 	mflo	$s7
 	add	$s5,$t7,$s7	#s5 is x[b]
+
+
 	l.d	$f0,($s5)
-	s.d	$f0,($s4)	
+	s.d	$f0,($s4)
+	
 # 	s.d	$s5,($s4)
 # 	s.d	$f0,
+
 	mov.d	$f12,$f0
-	syscall	$print_double				#             //            x[b] = t;
+
+	syscall	$print_double
+
+				#             //            x[b] = t;
 	l.d	$f0,t
-	s.d	$f0,($s5)	
+	s.d	$f0,($s5)
+	
+	
 swap:				#             swap(x[a],x[b]);
 	#loading x[b]
 	li	$s7,16
-	mul	$s7,$s7,$t3	
+	mul	$s7,$s7,$t3
+	
 	add	$s4,$t7,$s7	#s4 is x[b]
+
 	#loading x[a]
 	li	$s7,16
 	mul	$s7,$s7,$t8
-	add	$s5,$t7,$s7	#s5 is x[a]	
+	add	$s5,$t7,$s7	#s5 is x[a]
+	
 	#loading x[a] and x[b] into the fpu 
 	l.d	$f2,($s4)
 	l.d	$f0,($s5)
+
 	#reversing the storage loactions from fpu
 	s.d	$f0,($s4)		#completes swap
 	s.d	$f2,($s5)
+
 				#         }
 2:
+	
+
 	j	Decimate	
+
 FinishDecimate:	
 		#     }
-				
+				#     //// Normalize (This section make it not working correctly)
+				#     Complex f = 1.0 / N;
+				#     for (unsigned int i = 0; i < N; i++)
+				#         x[i] *= f;
+				# }
 98:	lw	$ra,fp.ra($fp)	# restore registers from stack
 	lw	$s0,fp.s0($fp)
 	lw	$s1,fp.s1($fp)
@@ -719,12 +952,20 @@ FinishDecimate:
 	lw	$fp,fp.fp($fp)
 	add	$sp,$sp,fp.a4-fp.s4
 	jr	$ra
+
+
+
+
 	.data
 
 complestruct:	.space	complex
 file1:		.space	file	
 
 #s0 contains file handle, a1 contains buffer, a2 length to be read
+
+
+
+
 
 #get a phasor input 
 # gtive it a magniatude and angle convert that to complex 
@@ -740,27 +981,70 @@ file1:		.space	file
 	.code
 
 main:
+
 # 	la	$s0,file1
 # 	add	$a0,$s0,file.filename
+	
+
 	la	$a0,file.name
 	addi	$a1,_O_RDONLY
 # 	li	$a2,0
-	mov	$s0,$v0		
+
+	mov	$s0,$v0
+	
+	
 # 	syscall	$open
 # 	jal	file.readfile
+
+
+
+
+
+
+
+
 # 	jal	readfile
 	#thing I need to do reading command 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
 	jal	complex.Cooley_Tukey_FFT
 # 	
 	li	$a0,'\n
 	syscall	$print_char
+
 	li	$a0,'\n
 	syscall	$print_char
+
 	li	$a0,'\n
 	syscall	$print_char
+
 	li	$v0,0
 	li	$t0,8
 	la	$a0,Data
+
 1:
 	beq	$v0,$t0,end
 	l.d	$f12,($a0)
@@ -772,6 +1056,7 @@ main:
 	addi	$a0,16
 	addi	$v0,1
 	j	1b
+
 end:
 
 
